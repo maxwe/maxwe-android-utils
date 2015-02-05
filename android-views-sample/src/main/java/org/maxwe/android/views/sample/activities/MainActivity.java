d4edd5b363390dde4f35c0d1bda2a4b8e5ba6297
DingPengwei <www.dingpengwei@gmail.com>
2015-02-05 16:47:53 +0800

    part of image view

--- android-views-sample/src/main/java/org/maxwe/android/views/sample/activities/MainActivity.java
+++ android-views-sample/src/main/java/org/maxwe/android/views/sample/activities/MainActivity.java
@@ -1,23 +1,35 @@
 package org.maxwe.android.views.sample.activities;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import org.maxwe.android.views.image.MulitImage;
+import org.maxwe.android.views.sample.R;
 import org.maxwe.android.views.treelist.TreeListView;
+
+import java.util.LinkedList;
 
 /**
  * Created by dingpengwei on 10/23/14.
  */
 public class MainActivity extends Activity {
 
-    private TreeListView treeListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.treeListView = new TreeListView(this);
-        this.setContentView(this.treeListView);
+        LinkedList<Integer> integers = new LinkedList<Integer>();
+        integers.add(R.drawable.ic_launcher);
+        integers.add(R.drawable.ic_launcher);
+        integers.add(R.drawable.ic_launcher);
+        integers.add(R.drawable.ic_launcher);
+        integers.add(R.drawable.ic_launcher);
+        integers.add(R.drawable.ic_launcher);
+        integers.add(R.drawable.ic_launcher);
+        MulitImage mulitImage = new MulitImage(this,integers);
+        this.setContentView(mulitImage);
     }
 
 
