db7566ad96d60690780834a31634b9ce95efde07
DingPengwei <www.dingpengwei@gmail.com>
2016-01-26 15:03:48 +0800

    hexagon scroller

--- android-utils-sample/src/main/java/org/maxwe/android/utils/sample/hexagon/HexagonActivity.java
+++ android-utils-sample/src/main/java/org/maxwe/android/utils/sample/hexagon/HexagonActivity.java
@@ -3,8 +3,16 @@ package org.maxwe.android.utils.sample.hexagon;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.Toast;
 import org.maxwe.android.utils.sample.R;
+import org.maxwe.android.utils.views.hexagon.HexagonContainer;
+import org.maxwe.android.utils.views.hexagon.HexagonView;
+
+import java.util.LinkedList;
 
 /**
  * Created by Pengwei Ding on 2016-01-10 16:20.
@@ -15,18 +23,26 @@ public class HexagonActivity extends Activity implements View.OnClickListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        LinkedList<HexagonView> hexagonItems = new LinkedList<>();
-//        int initNum = 15;
-//        for (int index =0 ;index<initNum;index++){
-//            HexagonView hexagonView = new HexagonView(this);
-//            hexagonView.setOnClickListener(this);
-//            ImageView imageView = new ImageView(this);
-//            imageView.setImageResource(R.drawable.index_01);
-//            hexagonView.addView(imageView);
-//            hexagonItems.add(hexagonView);
-//        }
-//        this.setContentView(new HexagonContainer(this, hexagonItems,4));
-        this.setContentView(R.layout.org_maxwe_android_utils_hexagon2);//152 5168 2555
+        LinkedList<HexagonView> hexagonItems = new LinkedList<>();
+        int initNum = 30;
+        for (int index =0 ;index<initNum;index++){
+            HexagonView hexagonView = new HexagonView(this);
+            RelativeLayout relativeLayout = new RelativeLayout(this);
+            relativeLayout.setBackgroundResource(R.drawable.org_maxwe_hexagon_bg);
+            ImageView imageView = new ImageView(this);
+            imageView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT));
+            imageView.setImageResource(R.drawable.index_01);
+            relativeLayout.addView(imageView);
+            hexagonView.setOnClickListener(this);
+            hexagonView.addView(relativeLayout);
+            hexagonItems.add(hexagonView);
+        }
+        ScrollView scrollView = new ScrollView(this);
+        HexagonContainer hexagonContainer = new HexagonContainer(this, hexagonItems, 4);
+        hexagonContainer.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        scrollView.addView(hexagonContainer);
+        this.setContentView(scrollView);
+//        this.setContentView(R.layout.org_maxwe_android_utils_hexagon2);//152 5168 2555
     }
 
     @Override
--- android-utils-views/src/main/java/org/maxwe/android/utils/views/hexagon/HexagonContainer.java
+++ android-utils-views/src/main/java/org/maxwe/android/utils/views/hexagon/HexagonContainer.java
@@ -15,17 +15,25 @@ import java.util.LinkedList;
  */
 public class HexagonContainer extends RelativeLayout {
     private LinkedList<HexagonView> hexagonViews;
-    private int containerWidth;
-    private int containerHeight;
+    private float containerWidth;
+    private float containerHeight;
     private int column;
+    private int childCount;
+    private float sideLength;
+    private float leftOffset = 0.0f;
+    private float topOffset = sideLength / 2 / 3;
 
     public HexagonContainer(Context context, LinkedList<HexagonView> hexagonViews, int column) {
         super(context);
         this.hexagonViews = hexagonViews;
         this.column = column;
+        if (this.column < 1) {
+            this.column = 1;
+        }
         for (HexagonView hexagonView : hexagonViews) {
             this.addView(hexagonView);
         }
+//        this.childCount = this.getChildCount();
     }
 
     public HexagonContainer(Context context, AttributeSet attrs) {
@@ -41,6 +49,9 @@ public class HexagonContainer extends RelativeLayout {
     private void initAttr(AttributeSet attrs) {
         TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.HexagonContainer);
         this.column = typedArray.getInteger(R.styleable.HexagonContainer_column, 1);
+        if (this.column < 1) {
+            this.column = 1;
+        }
     }
 
 
@@ -48,25 +59,35 @@ public class HexagonContainer extends RelativeLayout {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         this.containerWidth = MeasureSpec.getSize(widthMeasureSpec);
-        this.containerHeight = MeasureSpec.getSize(heightMeasureSpec);
-//        this.setMeasuredDimension(this.containerWidth,this.containerHeight);
+        this.sideLength = this.containerWidth / this.column;
+//        float leftOffset = (sideLength / 2 - (float) (sideLength / 2 * Math.cos(Math.PI / 6))) / 2;
+        /**
+         * 定义顶部的偏移量为六分之一的单位高度
+         */
+        this.topOffset = sideLength / 2 / 3;
+        /**
+         * childCount%column余数为零则childCount/column取整后不再添加新行高
+         * childCount%column余数大于0小于等于column则childCount/column取整后再添加一行新行高
+         * childCount%column余数大于column则childCount/column取整后再添加两行新行高
+         */
+        int multiple = this.childCount % (this.column * 2 - 1) > 4 ? 2 : this.childCount % (this.column * 2 - 1) > 0 ? 1 : 0;
+        float newLineHeight = this.sideLength * multiple;
+        /**
+         * 总体高度 = 行高 + 余行高 - 高度偏移量的累计值
+         */
+        this.containerHeight = this.childCount / (this.column * 2 - 1) * (this.sideLength - this.topOffset) * 2 + ((newLineHeight == 0)?this.topOffset:newLineHeight);
+        this.setMeasuredDimension((int) this.containerWidth, (int) this.containerHeight);
     }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        super.onLayout(changed,l,t,r,b);
-        int childCount = this.getChildCount();
-        float sideLength = this.containerWidth / this.column;
-//        float leftOffset = (sideLength / 2 - (float) (sideLength / 2 * Math.cos(Math.PI / 6))) / 2;
-        float leftOffset = 0.0f;
-        float topOffset = sideLength / 2 / 3;
-
+        super.onLayout(changed, l, t, r, b);
         float startX = 0;
         float startY = 0;
-
+        this.childCount = this.getChildCount();
         for (int index = 0; index < childCount; index++) {
             HexagonView childAt = (HexagonView) this.getChildAt(index);
-            childAt.measure(MeasureSpec.makeMeasureSpec((int)sideLength,MeasureSpec.EXACTLY), MeasureSpec.makeMeasureSpec((int)sideLength,MeasureSpec.EXACTLY));
+            childAt.measure(MeasureSpec.makeMeasureSpec((int) sideLength, MeasureSpec.EXACTLY), MeasureSpec.makeMeasureSpec((int) sideLength, MeasureSpec.EXACTLY));
             childAt.layout((int) startX, (int) startY, (int) (startX + sideLength), (int) (startY + sideLength));
             System.out.println();
             if (startX + sideLength * 2 > this.containerWidth) {
