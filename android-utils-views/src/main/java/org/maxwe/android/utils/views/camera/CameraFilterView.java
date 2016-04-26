57cb1e08e703d33100501341bc3d57c16fa9c56b
DingPengwei <www.dingpengwei@gmail.com>
2016-04-26 16:57:59 +0800

    stash

--- android-utils-views/src/main/java/org/maxwe/android/utils/views/camera/CameraFilterView.java
+++ android-utils-views/src/main/java/org/maxwe/android/utils/views/camera/CameraFilterView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.SurfaceTexture;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
 import android.util.AttributeSet;
 
 import javax.microedition.khronos.egl.EGLConfig;
@@ -31,18 +32,6 @@ public class CameraFilterView extends GLSurfaceView implements GLSurfaceView.Ren
         this.setRenderMode(RENDERMODE_WHEN_DIRTY);
     }
 
-
-//    @Override
-//    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-//    }
-//    @Override
-//    public void onSurfaceChanged(GL10 gl, int width, int height) {
-//    }
-//    @Override
-//    public void onDrawFrame(GL10 gl) {
-//    }
-
-
     private Triangle mTriangle;
     @Override
     public void onFrameAvailable(SurfaceTexture surfaceTexture) {
@@ -56,17 +45,32 @@ public class CameraFilterView extends GLSurfaceView implements GLSurfaceView.Ren
         this.mTriangle = new Triangle();
     }
 
+
+    // mMVPMatrix is an abbreviation for "Model View Projection Matrix"
+    private final float[] mMVPMatrix = new float[16];
+    private final float[] mProjectionMatrix = new float[16];
+    private final float[] mViewMatrix = new float[16];
+
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
         GLES20.glViewport(0, 0, width, height);
+        float ratio = (float) width / height;
+
+        // this projection matrix is applied to object coordinates
+        // in the onDrawFrame() method
+        Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
     }
 
 
     @Override
     public void onDrawFrame(GL10 gl) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-        mTriangle.draw();
-
+        // Set the camera position (View matrix)
+//        Matrix.setLookAtM(mViewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+        Matrix.setLookAtM(mViewMatrix, 0, 0, 0, 3.0f, 0f, 0f, 0f, 0f, 0f, 0.0f);
+        // Calculate the projection and view transformation
+        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
+        mTriangle.draw(mMVPMatrix);
     }
 
     @Override
--- android-utils-views/src/main/java/org/maxwe/android/utils/views/camera/Triangle.java
+++ android-utils-views/src/main/java/org/maxwe/android/utils/views/camera/Triangle.java
@@ -24,15 +24,33 @@ public class Triangle {
     };
 
 
+//    static float triangleCoords[] = {   // in counterclockwise order:
+//            0.0f,  0.622008459f, 0.0f, // top
+//            -0.5f, -0.311004243f, 0.0f, // bottom left
+//            0.5f, -0.311004243f, 0.0f  // bottom right
+//    };
+
+
     // Set color with red, green, blue and alpha (opacity) values
     float color[] = { 0.63671875f, 0.76953125f, 0.22265625f, 1.0f };
 
 
     private final String vertexShaderCode =
+            // This matrix member variable provides a hook to manipulate
+            // the coordinates of the objects that use this vertex shader
+            "uniform mat4 uMVPMatrix;" +
             "attribute vec4 vPosition;" +
                     "void main() {" +
-                    "  gl_Position = vPosition;" +
+                    // the matrix must be included as a modifier of gl_Position
+                    // Note that the uMVPMatrix factor *must be first* in order
+                    // for the matrix multiplication product to be correct.
+//                    "  gl_Position = vPosition;" +
+                    "  gl_Position = uMVPMatrix * vPosition;" +
                     "}";
+
+    // Use to access and set the view transformation
+    private int mMVPMatrixHandle;
+
 
     private final String fragmentShaderCode =
             "precision mediump float;" +
@@ -47,6 +65,9 @@ public class Triangle {
 
     private final int vertexCount = triangleCoords.length / COORDS_PER_VERTEX;
     private final int vertexStride = COORDS_PER_VERTEX * 4; // 4 bytes per vertex
+
+
+
 
     public Triangle() {
         // initialize vertex byte buffer for shape coordinates
@@ -74,7 +95,10 @@ public class Triangle {
         GLES20.glLinkProgram(mProgram);
     }
 
-    public void draw() {
+
+
+    public void draw(float[] mvpMatrix) {
+
         // Add program to OpenGL ES environment
         GLES20.glUseProgram(mProgram);
         // get handle to vertex shader's vPosition member
@@ -83,13 +107,25 @@ public class Triangle {
         GLES20.glEnableVertexAttribArray(mPositionHandle);
         // Prepare the triangle coordinate data
         GLES20.glVertexAttribPointer(mPositionHandle, COORDS_PER_VERTEX, GLES20.GL_FLOAT, false, vertexStride, vertexBuffer);
-        // get handle to fragment shader's vColor member
-        mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
-        // Set color for drawing the triangle
-        GLES20.glUniform4fv(mColorHandle, 1, color, 0);
+
+
+//        // get handle to fragment shader's vColor member
+//        mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
+//        // Set color for drawing the triangle
+//        GLES20.glUniform4fv(mColorHandle, 1, color, 0);
+
+        // get handle to shape's transformation matrix
+        mMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
+        // Pass the projection and view transformation to the shader
+        GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mvpMatrix, 0);
+
         // Draw the triangle
         GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
         // Disable vertex array
         GLES20.glDisableVertexAttribArray(mPositionHandle);
+
+
+
+
     }
 }
